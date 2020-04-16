2a9aee1ce3ccdb82adc56d2a5d074d81564c3e2c
Winston <winston.c1@samsung.com>
2020-04-16 18:40:30 -0400

    added some css

--- warehouse/warehouse/settings.py
+++ warehouse/warehouse/settings.py
@@ -145,3 +145,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+STATICFILES_DIRS = (
+  os.path.join(SITE_ROOT, 'static/'),
+)
